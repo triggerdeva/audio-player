cb91b59d9247b000d49286fb9327e1aad3f86664
triggerdeva <120394539+triggerdeva@users.noreply.github.com>
2023-05-11 13:54:58 +0530

    done

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-/* when the projects initally loads we have to make api calls to get data */
+const data = new Map([]);
+/******* 
+  fetching data
+*********/
 import { getPopularArtist, getTopAlbums, getTopTracks, getTopPlaylist,searchSong } from "./api.js";
 const getHomePageData = async () => {
     console.log("fetching data");
@@ -14,7 +17,9 @@ const getHomePageData = async () => {
     }
 }
 
-// html elements selectors
+/******* 
+  html elements selectors
+*********/
 
 const homeLink = document.getElementById("home-link");
 const collectionLink = document.getElementById("collection-link");
@@ -23,11 +28,21 @@ const pageContainer = document.getElementById("page-container");
 const homePage = document.getElementById("home-page");
 const collectionPage = document.getElementById("collection-page");
 const historyPage = document.getElementById("history-page");
-
+const homeLoader = document.getElementById("home-loader");
+const collectionLoader = document.getElementById("collection-loader");
+const historyLoader = document.getElementById("history-loader");
+const homePageContent = document.getElementById("home-page-content");
+const collectionPageContent = document.getElementById("collection-page-content");
+const historyPageContent = document.getElementById("history-page-content");
+const topTracksContainer = document.getElementById("top-tracks-container");
+const topAlbumsContainer = document.getElementById("top-albums-container");
+const topArtistsContainer = document.getElementById("top-artists-container");
+const topPlaylistsContainer = document.getElementById("top-playlists-container");
 
 /******* 
   page router
 *********/
+
 (() => {
     console.log("setting up page router");
     homeLink.addEventListener("click", (e) => {
@@ -56,6 +71,10 @@ const historyPage = document.getElementById("history-page");
     })
 })()
 
+/******* 
+  render home page
+*********/
+
 const renderHomePage = async () => {
     const {
         tracks,
@@ -63,19 +82,80 @@ const renderHomePage = async () => {
         albums,
         playlists
     } = await getHomePageData()
-    while(homePage.firstChild){
-        homePage.firstChild.remove();
-    }
-    console.log(
-        tracks,
-        artists,
-        albums,
-        playlists
-    )
+    renderTracks(topTracksContainer,await tracks.data )
+    renderArtists(topArtistsContainer, await artists.data)
+    renderAlbums(topAlbumsContainer, await albums.data )
+    renderPlaylists(topPlaylistsContainer, await playlists.data)
+    homeLoader.classList.add("hide")
+    homePageContent.classList.remove("hide")
 }
+
+async function renderTracks(container,tracksList){
+    const {tracks} = tracksList;
+    console.log("rendering ", tracks)
+    tracks.forEach(track => {
+        const htmlString = getCardHtml(track);
+        container.insertAdjacentHTML("beforeend",htmlString)
+    });
+    console.log("current data is", data);
+}
+async function renderAlbums(container,albumList){
+    console.log("rendering ",albumList)
+    const {albums} = albumList;
+    console.log("rendering ", albums)
+    albums.forEach(album => {
+        const htmlString = getAlbumHtml(album);
+        container.insertAdjacentHTML("beforeend",htmlString)
+    });
+    
+}
+async function renderPlaylists(container,playlistList){
+    console.log("rendering ",playlistList)
+    // const {tracks : tracksList} = tracksList;
+    // console.log("rendering ", tracksList)
+    // tracksList.forEach(track => {
+    //     const cardHtml = getCardHtml(track);
+    //     container.insertAdjacentHTML("beforeend",cardHtml)
+    // });
+    
+}
+async function renderArtists(container,artistList){
+    console.log("rendering ",artistList)
+    // const {tracks : tracksList} = tracksList;
+    // console.log("rendering ", tracksList)
+    // tracksList.forEach(track => {
+    //     const cardHtml = getCardHtml(track);
+    //     container.insertAdjacentHTML("beforeend",cardHtml)
+    // });
+
+}
+
+function getAlbumHtml(album){
+    console.log("get albumHtml",album)
+    const htmlString = `
+
+    `;
+    return htmlString;
+}
+
+function getCardHtml(track){
+    data.set(track.id,track);
+    const htmlString = `
+    <div class="track-card">
+        <img class="track-image" src="https://api.napster.com/imageserver/v2/albums/${track.albumId}/images/500x500.jpg" alt="">
+        <div class="track-card-content">
+            <h3>${track.albumName}</h3>
+            <button data-action="play" data-id="${track.id}" class="play"><img src="./images/play-button.png" alt=""></button>
+            <button data-action="playlist" data-id="${track.id}" class="add-to-playlist"><img src="./images/playlist.png" alt=""></button>
+            <button data-action="queue" data-id="${track.id}" class="add-to-queue"><img src="./images/queue.png" alt=""></button>
+        </div>
+    </div>
+    `;
+    return htmlString;
+}
+
+/********
+ * loading the app
+ */
 renderHomePage();
 
-
-// const handleClick = async () => {
-
-// }
