b62d3902db4842e638ecfde2f039425e2f490967
vinit-churi <vinitchuri@hotmail.com>
2023-05-18 20:36:20 +0530

    render artist

--- api.js
+++ api.js
@@ -1,67 +1,74 @@
 const API_KEY = "MWE1MjlmMzEtMjNiOC00NzU1LWI2MTYtZmMyZjUzYzUyOWIz";
 export const getTopTracks = async () => {
-    try{
+    try {
         const fetchURL = `https://napi-v2-2-cloud-run-b3gtd5nmxq-uw.a.run.app/v2.2/tracks/top?apikey=${API_KEY}&limit=10`;
-        const data = await fetch(fetchURL)
-        return {data : data.json(), error : null};
-    }catch(error){
-        console.log(error)
-        return  {data : null, error : error}
-    } 
-}
+        const data = await fetch(fetchURL);
+        return { data: data.json(), error: null };
+    } catch (error) {
+        console.log(error);
+        return { data: null, error: error };
+    }
+};
 
 export const getAlbumTracks = async (link) => {
-    try{
-        const data = await fetch(`${link}?apikey=${API_KEY}`)
-        return {data : data.json(), error : null};
-    }catch(error){
-        console.log(error)
-        return  {data : null, error : error}
-    } 
-}
+    try {
+        const data = await fetch(`${link}?apikey=${API_KEY}`);
+        return { data: data.json(), error: null };
+    } catch (error) {
+        console.log(error);
+        return { data: null, error: error };
+    }
+};
+export const getArtistTracks = async (link) => {
+    try {
+        const data = await fetch(`${link}?apikey=${API_KEY}`);
+        return { data: data.json(), error: null };
+    } catch (error) {
+        console.log(error);
+        return { data: null, error: error };
+    }
+};
 export const getPopularArtist = async () => {
-    try{
+    try {
         const fetchURL = `https://napi-v2-2-cloud-run-b3gtd5nmxq-uw.a.run.app/v2.2/artists/top?apikey=${API_KEY}&limit=10`;
-        const data = await fetch(fetchURL)
-        return {data : data.json(), error : null};
-    }catch(error){
-        console.log(error)
-        return  {data : null, error : error}
+        const data = await fetch(fetchURL);
+        return { data: data.json(), error: null };
+    } catch (error) {
+        console.log(error);
+        return { data: null, error: error };
     }
-    
-}
+};
 export const getTopPlaylist = async () => {
-    try{
+    try {
         const fetchURL = `https://napi-v2-2-cloud-run-b3gtd5nmxq-uw.a.run.app/v2.2/playlists/featured?apikey=${API_KEY}&limit=10`;
-        const data = await fetch(fetchURL)
-        return {data : data.json(), error : null};
-    }catch(error){
-        console.log(error)
-        return  {data : null, error : error}
+        const data = await fetch(fetchURL);
+        return { data: data.json(), error: null };
+    } catch (error) {
+        console.log(error);
+        return { data: null, error: error };
     }
-    
-}
+};
 export const getTopAlbums = async () => {
-    try{
+    try {
         const fetchURL = `https://napi-v2-2-cloud-run-b3gtd5nmxq-uw.a.run.app/v2.2/albums/new?apikey=${API_KEY}&limit=4`;
-        const data = await fetch(fetchURL)
-        return {data : data.json(), error : null};
-    }catch(error){
-        console.log(error)
-        return  {data : null, error : error}
+        const data = await fetch(fetchURL);
+        return { data: data.json(), error: null };
+    } catch (error) {
+        console.log(error);
+        return { data: null, error: error };
     }
-}
+};
 
 export const searchSong = async (query, type) => {
-    try{
+    try {
         //  album, artist, track, tag, playlist.
-        const fetchURL = "https://napi-v2-2-cloud-run-b3gtd5nmxq-uw.a.run.app/v2.2/search?apikey=${API_KEY}&per_type_limit=5"; 
-        if(type && type !== "all")
-            fetchURL += `&type=${type}`; 
-        const data = await fetch(fetchURL)
-        return {data : data.json(), error : null};
-    }catch(error){
-        console.log(error)
-        return  {data : null, error : error}
+        const fetchURL =
+            "https://napi-v2-2-cloud-run-b3gtd5nmxq-uw.a.run.app/v2.2/search?apikey=${API_KEY}&per_type_limit=5";
+        if (type && type !== "all") fetchURL += `&type=${type}`;
+        const data = await fetch(fetchURL);
+        return { data: data.json(), error: null };
+    } catch (error) {
+        console.log(error);
+        return { data: null, error: error };
     }
-}
+};
--- main.js
+++ main.js
@@ -2,20 +2,28 @@ const data = new Map([]);
 /******* 
   fetching data
 *********/
-import { getPopularArtist, getTopAlbums, getTopTracks, getTopPlaylist,searchSong,getAlbumTracks } from "./api.js";
+import {
+    getPopularArtist,
+    getTopAlbums,
+    getTopTracks,
+    getTopPlaylist,
+    searchSong,
+    getAlbumTracks,
+    getArtistTracks,
+} from "./api.js";
 const getHomePageData = async () => {
     console.log("fetching data");
-    const tracks = await getTopTracks()
-    const artists = await getPopularArtist()
+    const tracks = await getTopTracks();
+    const artists = await getPopularArtist();
     const albums = await getTopAlbums();
     const playlists = await getTopPlaylist();
     return {
         tracks,
         artists,
         albums,
-        playlists
-    }
-}
+        playlists,
+    };
+};
 
 /******* 
   html elements selectors
@@ -32,12 +40,16 @@ const homeLoader = document.getElementById("home-loader");
 const collectionLoader = document.getElementById("collection-loader");
 const historyLoader = document.getElementById("history-loader");
 const homePageContent = document.getElementById("home-page-content");
-const collectionPageContent = document.getElementById("collection-page-content");
+const collectionPageContent = document.getElementById(
+    "collection-page-content"
+);
 const historyPageContent = document.getElementById("history-page-content");
 const topTracksContainer = document.getElementById("top-tracks-container");
 const topAlbumsContainer = document.getElementById("top-albums-container");
 const topArtistsContainer = document.getElementById("top-artists-container");
-const topPlaylistsContainer = document.getElementById("top-playlists-container");
+const topPlaylistsContainer = document.getElementById(
+    "top-playlists-container"
+);
 
 /******* 
   page router
@@ -46,112 +58,121 @@ const topPlaylistsContainer = document.getElementById("top-playlists-container")
 (() => {
     console.log("setting up page router");
     homeLink.addEventListener("click", (e) => {
-        homeLink.classList.add("active-link")
-        collectionLink.classList.remove("active-link")
-        historyLink.classList.remove("active-link")
-        collectionPage.classList.add("hide-page")
-        historyPage.classList.add("hide-page")
-        homePage.classList.remove("hide-page")
-    })
+        homeLink.classList.add("active-link");
+        collectionLink.classList.remove("active-link");
+        historyLink.classList.remove("active-link");
+        collectionPage.classList.add("hide-page");
+        historyPage.classList.add("hide-page");
+        homePage.classList.remove("hide-page");
+    });
     collectionLink.addEventListener("click", (e) => {
-        collectionLink.classList.add("active-link")
-        homeLink.classList.remove("active-link")
-        historyLink.classList.remove("active-link")
-        homePage.classList.add("hide-page")
-        historyPage.classList.add("hide-page")
-        collectionPage.classList.remove("hide-page")
-    })
+        collectionLink.classList.add("active-link");
+        homeLink.classList.remove("active-link");
+        historyLink.classList.remove("active-link");
+        homePage.classList.add("hide-page");
+        historyPage.classList.add("hide-page");
+        collectionPage.classList.remove("hide-page");
+    });
     historyLink.addEventListener("click", (e) => {
-        historyLink.classList.add("active-link")
-        homeLink.classList.remove("active-link")
-        collectionLink.classList.remove("active-link")
-        homePage.classList.add("hide-page")
-        collectionPage.classList.add("hide-page")
-        historyPage.classList.remove("hide-page")
-    })
-})()
+        historyLink.classList.add("active-link");
+        homeLink.classList.remove("active-link");
+        collectionLink.classList.remove("active-link");
+        homePage.classList.add("hide-page");
+        collectionPage.classList.add("hide-page");
+        historyPage.classList.remove("hide-page");
+    });
+})();
 
 /******* 
   render home page
 *********/
 
 const renderHomePage = async () => {
-    const {
-        tracks,
-        artists,
-        albums,
-        playlists
-    } = await getHomePageData()
-    renderTracks(topTracksContainer,await tracks.data )
-    renderArtists(topArtistsContainer, await artists.data)
-    renderAlbums(topAlbumsContainer, await albums.data )
-    renderPlaylists(topPlaylistsContainer, await playlists.data)
-    homeLoader.classList.add("hide")
-    homePageContent.classList.remove("hide")
-}
+    const { tracks, artists, albums, playlists } = await getHomePageData();
+    renderTracks(topTracksContainer, await tracks.data);
+    renderArtists(topArtistsContainer, await artists.data);
+    renderAlbums(topAlbumsContainer, await albums.data);
+    homeLoader.classList.add("hide");
+    homePageContent.classList.remove("hide");
+};
 
-async function renderTracks(container,tracksList){
-    const {tracks} = tracksList;
-    tracks.forEach(track => {
-        container.append(getCardElement(track))
+async function renderTracks(container, tracksList) {
+    const { tracks } = tracksList;
+    tracks.forEach((track) => {
+        container.append(getCardElement(track));
     });
     console.log("current data is", data);
 }
-function renderAlbums(container,albumList){
-    const {albums} = albumList;
-    console.log("function 1",container,albumList);
+function renderAlbums(container, albumList) {
+    const { albums } = albumList;
+    console.log("function 1", container, albumList);
     albums.forEach(async (album) => {
-        let albumElement = await getAlbum(album)
-        container.append(albumElement)
+        let albumElement = await getAlbum(album);
+        container.append(albumElement);
     });
-    
-}
-async function renderPlaylists(container,playlistList){
-    console.log("rendering ",playlistList)
-    // const {tracks : tracksList} = tracksList;
-    // console.log("rendering ", tracksList)
-    // tracksList.forEach(track => {
-    //     const cardHtml = getCardHtml(track);
-    //     container.append("beforeend",cardHtml)
-    // });
-    
-}
-async function renderArtists(container,artistList){
-    console.log("rendering ",artistList)
-    // const {tracks : tracksList} = tracksList;
-    // console.log("rendering ", tracksList)
-    // tracksList.forEach(track => {
-    //     const cardHtml = getCardHtml(track);
-    //     container.append("beforeend",cardHtml)
-    // });
-
 }
 
-async function getAlbum(album){
-    const { links:{tracks : {href : link}}} = album
-    console.log("function 2", link)
+async function renderArtists(container, artistList) {
+    const { artists } = artistList;
+    artists.forEach(async (artist) => {
+        let artistElement = await getArtist(artist);
+        container.append(artistElement);
+    });
+}
+
+async function getArtist(artist) {
+    console.log("look here", artist);
+    const {
+        links: {
+            topTracks: { href: link },
+        },
+    } = artist;
+    const artistFragment = await createArtistFragment(link);
+    return artistFragment;
+}
+async function getAlbum(album) {
+    const {
+        links: {
+            tracks: { href: link },
+        },
+    } = album;
+    console.log("function 2", link);
     const albumFragment = await createAlbumFragment(link);
     return albumFragment;
 }
 
-async function createAlbumFragment(link){
-    const {data, error} = await getAlbumTracks(link);
-    const {tracks} =  await data;
-    console.log("function 3", tracks)
+async function createArtistFragment(link) {
+    const { data, error } = await getArtistTracks(link);
+    const { tracks } = await data;
+
+    const container = document.createElement("div");
+    container.classList.add("individual-artist");
+    tracks.forEach(async (track) => {
+        const trackCard = await getCardElement(track);
+        container.append(trackCard);
+    });
+    console.log(container);
+    return container;
+}
+
+async function createAlbumFragment(link) {
+    const { data, error } = await getAlbumTracks(link);
+    const { tracks } = await data;
+    console.log("function 3", tracks);
 
     const container = document.createElement("div");
     container.classList.add("individual-album");
     tracks.forEach(async (track) => {
-        const trackCard = await getCardElement(track)
-        container.append(trackCard)
-    })
-    console.log(container)
+        const trackCard = await getCardElement(track);
+        container.append(trackCard);
+    });
+    console.log(container);
     return container;
 }
 
-function getCardElement(track){
+function getCardElement(track) {
     // console.log(data)
-    data.set(track.id,track);
+    data.set(track.id, track);
     const template = document.createElement("template");
     template.innerHTML = `
     <div class='track-card'>
@@ -165,33 +186,38 @@ function getCardElement(track){
     </div>
     `;
     const element = template.content.cloneNode(true);
-    element.querySelector(".track-image").src = `https://api.napster.com/imageserver/v2/albums/${track.albumId}/images/500x500.jpg` 
-    element.querySelector(".track-title").innerText =  track.name
-    const playButton = element.querySelector(".play"); 
-    const queueButton = element.querySelector(".add-to-playlist")
-    const playlistButton = element.querySelector(".add-to-queue")
-    playButton.dataset.id = track.id 
-    queueButton.dataset.id = track.id 
-    playlistButton.dataset.id = track.id 
+    element.querySelector(
+        ".track-image"
+    ).src = `https://api.napster.com/imageserver/v2/albums/${track.albumId}/images/500x500.jpg`;
+    element.querySelector(".track-title").innerText = track.name;
+    const playButton = element.querySelector(".play");
+    const queueButton = element.querySelector(".add-to-playlist");
+    const playlistButton = element.querySelector(".add-to-queue");
+    playButton.dataset.id = track.id;
+    queueButton.dataset.id = track.id;
+    playlistButton.dataset.id = track.id;
     playButton.addEventListener("click", (e) => {
         console.log(track.id);
-    })
+    });
     queueButton.addEventListener("click", (e) => {
         console.log(track.id);
-    })
+    });
     playlistButton.addEventListener("click", (e) => {
         console.log(track.id);
-    })
+    });
     return element;
 }
 
 /**********
  * Evnet listner
-***********/
-
+ ***********/
 
 /********
  * loading the app
  */
 renderHomePage();
 
+/********
+ * setting up the audio player
+ */
+const audioElement = new Audio();
